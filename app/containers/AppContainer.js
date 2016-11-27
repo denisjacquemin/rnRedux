4d9a1db7764d64d27bdb4d6f233a408e8f40b814
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-11-27 17:12:09 +0100

    adding ADD RECIPE button

--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -3,19 +3,45 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Button,
 } from 'react-native';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {ActionCreators} from '../actions';
 
 class AppContainer extends Component {
+  state: {
+    recipeCount: number;
+  };
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      recipeCount: 0
+    }
+  }
+
+  incrementRecipeCount() {
+    this.setState({
+      recipeCount: this.state.recipeCount + 1
+    })
+  }
+
   render () {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!!
         </Text>
+        <Text>
+          Recipe Count: {this.state.recipeCount}
+        </Text>
+        <Button
+          onPress= { ()=> {this.incrementRecipeCount()} }
+          title="Add recipe"
+          color="#841584"
+        />
         <Text style={styles.instructions}>
           To get started, edit AppContainer
         </Text>
